615c578b29117f48c15322497b5c20d73f6b1ce4
talyt <talyta_vinente@hotmail.com>
2023-10-27 13:34:41 -0300

    desenvolvimento do desafio

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,24 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+val nomeHeroi = readln()!!.toString()
+val xp = readln()!!.toDouble()
+var nivel = ""
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+
+    when {
+        xp <= 1.000 -> nivel = "Ferro"
+        xp >= 1.001 && xp == 2.000 -> nivel = "Bronze"
+        xp >= 2.001 && xp == 5.000 -> nivel = "Prata"
+        xp >= 5.001 && xp == 6.000 -> nivel = "Ouro"
+        xp >= 6.001 && xp == 7.000 -> nivel = "Platina"
+        xp >= 7.001 && xp == 8.000 -> nivel = "Diamante"
+        xp >= 8.001 && xp == 9.000 -> nivel = "Ascendente"
+        xp >= 9.001 && xp == 10.000 -> nivel = "Imortal"
+        xp > 10.001 -> nivel = "Imortal"
+
+    }
+    println("O Herói de nome $nomeHeroi está no nível de $nivel")
+
 }
+
+
+
